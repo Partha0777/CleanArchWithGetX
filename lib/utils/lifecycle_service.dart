e857c8f58d38f02f15b327383b5ef683a5393677
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2024-03-19 23:19:09 +0530

    Implemented common life cycle service with GetX service.

---
+++ lib/utils/lifecycle_service.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class LifeCycleService extends GetxService with WidgetsBindingObserver{
+
+  @override
+  void onInit() {
+    WidgetsBinding.instance.addObserver(this);
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.onClose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+    super.didChangeAppLifecycleState(state);
+  }
+}
